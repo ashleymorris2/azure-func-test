eec1dd3a61d6546ea02b4a36a87b6c659432425a
Ashley Morris <Ashley.Morris@carfinance247.co.uk>
2021-10-20 11:35:31 +0100

    Got blob working

--- BlobTest.cs
+++ BlobTest.cs
@@ -1,19 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic.FileIO;
 
 namespace ServerlessFunc
 {
     public static class BlobTest
     {
         [FunctionName("BlobTest")]
-        public static async Task RunAsync([BlobTrigger("samples-workitems/{name}", Connection = "")] Stream myBlob,
-            string name, ILogger log)
+        public static async Task RunAsync([BlobTrigger("csvblobtest/{name}")] Stream myBlob, string name, ILogger log)
         {
+            var data = ParseCsv(myBlob);
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+
+
+        private static List<String[]> ParseCsv(Stream blob)
+        {
+            using TextFieldParser textFieldParser = new TextFieldParser(blob);
+            textFieldParser.TextFieldType = FieldType.Delimited;
+            textFieldParser.SetDelimiters(",");
+
+            var rows = new List<String[]>();
+
+            while (!textFieldParser.EndOfData)
+            {
+                rows.Add(textFieldParser.ReadFields());
+            }
+
+            return rows;
         }
     }
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
   - https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio
 - Azure function core tools is recommended:
   - https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v3%2Cwindows%2Ccsharp%2Cportal%2Cbash%2Ckeda
+
+- Azure Storage Explorer may be useful:
+  - https://azure.microsoft.com/en-us/features/storage-explorer/#overview
